9fd4d4e37a4d3a67c388668a3cd19e5f7ed062be
Leticia Orive <leticia.orive.contractor@bbva.com>
2023-08-02 13:46:36 +0200

    componente registro

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -7,8 +7,23 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
   constructor(private authService: AuthService) {}
-
+  onSubmit() {
+    // Llamar al método de registro del servicio de autenticación
+    this.authService.register(this.email, this.password, this.name).subscribe(
+      (response) => {
+        // Manejar la respuesta del servidor después del registro exitoso
+        console.log('Registro exitoso:', response);
+      },
+      (error) => {
+        // Manejar errores en caso de fallo en el registro
+        console.error('Error en el registro:', error);
+      }
+    );
+  }
   register() {
     // Lógica para obtener los datos del formulario de registro (por ejemplo, nombre, email y contraseña)
     const userData = {
@@ -18,7 +33,7 @@ export class RegisterComponent {
     };
 
     // Llamar al método register del servicio AuthService
-    this.authService.register(userData).subscribe(
+    this.authService.register(this.email, this.password, this.name).subscribe(
       (response) => {
         // Procesar la respuesta del servidor si el registro es exitoso
         console.log('Registro exitoso', response);
--- angular/src/app/services/auth.service.ts
+++ angular/src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Observable, map } from 'rxjs';
 })
 export class AuthService {
   private baseUrl = 'http://localhost:3000/api'; // Cambia la URL por la del servidor backend
-
+  private apiUrl = 'https://tu-servidor-backend.com/api'; // Reemplaza con la URL de tu servidor backend
   constructor(private http: HttpClient) {}
 
   // Método para iniciar sesión
@@ -22,15 +22,13 @@ export class AuthService {
   }
 
   // Método para registrarse
-  register(userData: any): Observable<any> {
-    const url = `${this.baseUrl}/register`; // Ruta para el registro en el servidor
-    return this.http.post<any>(url, userData).pipe(
-      map((response) => {
-        // Aquí puedes realizar cualquier procesamiento adicional de la respuesta del servidor antes de devolverla
-        return response;
-      })
-    );
+  register(email: string, password: string, name: string): Observable<any> {
+    // Tu lógica de registro aquí
+    // Aquí realizas la solicitud POST al servidor para el registro
+    const userData = { email, password, name }; // Datos del usuario a enviar al servidor
+    return this.http.post(`${this.apiUrl}/register`, userData);
   }
+
   logout(): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/logout`, {});
   }
