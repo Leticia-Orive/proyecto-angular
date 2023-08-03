516735b42b5721d0b8825e405f9635716e095e61
Leticia Orive <leticia.orive.contractor@bbva.com>
2023-08-03 12:01:09 +0200

    arreglos en registrar

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -10,7 +10,9 @@ export class RegisterComponent {
   name: string = '';
   email: string = '';
   password: string = '';
+
   constructor(private authService: AuthService) {}
+
   onSubmit() {
     // Llamar al método de registro del servicio de autenticación
     this.authService.register(this.email, this.password, this.name).subscribe(
@@ -21,26 +23,6 @@ export class RegisterComponent {
       (error) => {
         // Manejar errores en caso de fallo en el registro
         console.error('Error en el registro:', error);
-      }
-    );
-  }
-  register() {
-    // Lógica para obtener los datos del formulario de registro (por ejemplo, nombre, email y contraseña)
-    const userData = {
-      name: 'John Doe',
-      email: 'user@example.com',
-      password: 'secretpassword',
-    };
-
-    // Llamar al método register del servicio AuthService
-    this.authService.register(this.email, this.password, this.name).subscribe(
-      (response) => {
-        // Procesar la respuesta del servidor si el registro es exitoso
-        console.log('Registro exitoso', response);
-      },
-      (error) => {
-        // Manejar el error si el registro falla
-        console.error('Error en el registro', error);
       }
     );
   }
